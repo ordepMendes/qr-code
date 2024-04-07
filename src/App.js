dd194d694ff87a784f1348e5defdb74797e6e713
ordepMendes <pedrohh0012@hotmail.com>
2024-04-07 17:37:32 -0300

    feat: :sparkles: First-commit: funcionalidade de gerar QR-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 
 function App() {
+  const [qrCodeValue, setQrCodeValue] = useState('');
+  const [qrCodeLink, setQrCodeLink] = useState('');
+
+  function handleQrCode(e){
+    setQrCodeValue(e.target.value);
+    handleGenerate(e.target.value);
+  }
+
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3
+    }, function(err,url){
+      setQrCodeLink(url);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Gerador de Qr Code</h1>
+      <div className='qrcode-wrapped'>
+      <QRCode value={qrCodeValue} />
+      </div>
+      <input placeholder='Digite sua URL' className='input' value={qrCodeValue} onChange={(e) => handleQrCode(e)}/>
+      <a href={qrCodeLink} download={`qrcode.png`}>Baixar QrCode</a>
     </div>
-  );
+  )
 }
 
 export default App;
